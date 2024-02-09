9644b8633ae5a01ddf502eb3c078853c4d9c55f1
AGretskikh <gretskih@mail.ru>
2024-02-09 09:01:03 +0300

    Открыл свободный доступ по адресу: /swagger-ui/*

--- src/main/java/ru/job4j/shortcut/config/WebSecurity.java
+++ src/main/java/ru/job4j/shortcut/config/WebSecurity.java
@@ -32,7 +32,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
-                .antMatchers(HttpMethod.GET, API_DOCS_URL, REDIRECT_URL).permitAll()
+                .antMatchers(HttpMethod.GET, API_DOCS_URL, REDIRECT_URL, "/swagger-ui/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
